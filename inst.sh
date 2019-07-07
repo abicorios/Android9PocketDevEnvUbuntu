8c08f438f0024b4b00d7fee634bb49beeeb97a8d
abicorios <yaroslav.moroz91@gmail.com>
2019-07-07 23:51:50 +0300

    Create inst.sh

---
+++ inst.sh
@@ -0,0 +1,11 @@
+#!/data/data/com.termux/files/usr/bin/bash
+pr=`pwd`
+cd
+
+#Thank AnLinux
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Ubuntu/ubuntu.sh && bash ubuntu.sh
+
+cp $pr/postinst.sh ~/ubuntu-fs/root/postinst.sh
+cp $pr/vimrc ~/ubuntu-fs/root/.vimrc
+echo './postinst.sh'>>~/ubuntu-fs/root/.profile
+bash start-ubuntu.sh
