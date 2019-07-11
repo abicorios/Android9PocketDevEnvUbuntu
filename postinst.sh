2443d47a44e5d57b6fe02a9208d13265d673ebbd
abicorios <yaroslav.moroz91@gmail.com>
2019-07-11 12:58:50 +0300

    fix down ycmd server

--- postinst.sh
+++ postinst.sh
@@ -7,6 +7,6 @@ apt install -y vim python3 git build-essential cmake python3-dev clang
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
-python3 install.py --clang-completer --system-libclang
+python3 install.py --clang-completer
 cd
 rm -f postinst.sh
