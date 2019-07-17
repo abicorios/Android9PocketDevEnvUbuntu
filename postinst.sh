d6b1b1991b9db462c0bfe2d9d28ac187de263973
abicorios <yaroslav.moroz91@gmail.com>
2019-07-17 05:09:52 +0300

    change to install ycm by apt and vom

--- postinst.sh
+++ postinst.sh
@@ -3,10 +3,12 @@ sed -i '$d' .profile
 echo 'set -o vi'>>.profile
 apt update -y
 apt upgrade -y
-apt install -y vim python3 git build-essential cmake python3-dev clang
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
-cd ~/.vim/bundle/YouCompleteMe
-python3 install.py --clang-completer
+#apt install -y vim python3 git build-essential cmake python3-dev clang
+apt install -y vim-youcompleteme build-essential less
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#vim +PluginInstall +qall
+#cd ~/.vim/bundle/YouCompleteMe
+#python3 install.py --clang-completer
+vam install youcompleteme
 cd
 rm -f postinst.sh
